0ba3a6fec7f9bbbadd54c41085d34b360cc27c4c
YOUR NAME <erinpagemd@yahoo.com>
2015-04-26 12:56:34 -0500

    refactored to less than 100 lines

--- lib/Month.rb
+++ lib/Month.rb
@@ -4,38 +4,26 @@ class Month
   def initialize(month, year)
     @month = month.to_i
     @year = year.to_i
-    if @year < 1800 || @year > 3000
-      error_message
-      exit
-    elsif @month <= 0 || @month > 12
+    if @year < 1800 || @year > 3000 || @month <= 0 || @month > 12
       error_message
       exit
     end
-
   end
 
   def days?
     days_31 = [1, 3, 5, 7, 8, 10, 12]
     days_30 = [4, 6, 9, 11]
-    @year = @year.to_i
     leap_year = true if @year % 4 == 0
-    century_leap_year = true if @year % 100 == 0
-    quadcentury_leap_year = true if @year % 400 == 0
-    month = @month.to_i
-    if days_31.include?(month)
+    century_leap_year = true if @year % 100 == 0 && leap_year
+    quadcentury_leap_year = true if @year % 400 == 0 && century_leap_year
+    if days_31.include?(@month)
       31
-    elsif days_30.include?(month)
+    elsif days_30.include?(@month)
       30
-    elsif month == 2 && leap_year && !century_leap_year && !quadcentury_leap_year
+    elsif @month == 2 && leap_year && !century_leap_year || @month == 2 && quadcentury_leap_year
       29
-    elsif month == 2 && leap_year && century_leap_year && quadcentury_leap_year
-      29
-    elsif month == 2 && leap_year && century_leap_year && !quadcentury_leap_year
-      28
-    elsif month == 2 && !leap_year
-      28
     else
-      "something very wrong happened in Month.days?"
+      28
     end
   end
 
@@ -47,37 +35,13 @@ class Month
       m = @month
       y = @year
     end
-
-    # 3 = March
-    # 4 = April
-    # 5 = May
-    # 6 = June
-    # 7 = July
-    # 8 = August
-    # 9 = September
-    #10 = October
-    #11 = November
-    #12 = December
-    #13 = January
-    #14 = February
-
-    # 0 = Saturday
-    # 1 = Sunday
-    # 2 = Monday
-    # 3 = Tuesday
-    # 4 = Wednesday
-    # 5 = Thursday
-    # 6 = Friday
-
     #ZellersCongruence info found at http://en.wikipedia.org/wiki/Zeller's_congruence
     day_of_week = (1 + (((m + 1) * 26) / 10).floor + y + (y / 4).floor + (6 * (y / 100).floor) + (y / 400).floor) % 7
-
   end
 
   def name
     months = [nil, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    month_index = @month.to_i
-    month_name = months[month_index]
+    month_name = months[@month]
   end
 
   def grid
@@ -88,9 +52,7 @@ class Month
     week4 = ""
     week5 = ""
     week6 = ""
-
     arr = (1..days?).to_a
-
     if (2..6).include?(start_day_of_week)
       num = start_day_of_week - 1
       num.times do arr.unshift(" ")
@@ -99,20 +61,15 @@ class Month
       num = start_day_of_week + 6
       num.times do arr.unshift(" ")
       end
-    else
-      arr
     end
-
     arr[0..6].each {|n| week1 << "#{n}".center(3)}
     arr[7..13].each {|n| week2 << "#{n}".center(3)}
     arr[14..20].each {|n| week3 << "#{n}".center(3)}
     arr[21..27].each {|n| week4 << "#{n}".center(3)}
-
     string << "#{week1.rstrip}\n"
     string << "#{week2.rstrip}\n"
     string << "#{week3.rstrip}\n"
     string << "#{week4.rstrip}\n"
-
     if arr.length > 35
       arr[28..34].each {|n| week5 << "#{n}".center(3)}
       arr[35..-1].each {|n| week6 << "#{n}".center(3)}
@@ -136,5 +93,4 @@ class Month
   def error_message
     print "Date not in acceptable format/range."
   end
-
 end
